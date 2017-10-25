4e355aa07d03850274f98a7680924ede1057ec63
Michael Barnes <mbarnes@asumh.edu>
2017-10-25 13:02:19 -0500

    week 11 done?

---
+++ js/basic_javascript.js
@@ -0,0 +1,27 @@
+function alertBox() {
+  alert("Danger, Will Robinson. Danger!")
+};
+
+function promptBox() {
+  var name = prompt("Please enter your name: ");
+  document.getElementById("testArea").innerHTML = "Hello, " + name + "!";
+};
+
+function confirmBox() {
+  var confirmation;
+  if (confirm("Are we cleared to proceed?") == true) {
+    confirmation = "Confirmed. We are a go.";
+  }
+  else {
+    confirmation = "That's a negative, good buddy.";
+  }
+  document.getElementById("testArea").innerHTML = confirmation;
+};
+
+function danger(my_image) {
+  my_image.src = "./images/danger.png";
+};
+
+function safe(my_image) {
+  my_image.src= "./images/safe.png";
+}
